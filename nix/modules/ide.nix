aa30b6f69c4c8de372cb98ff20df4c5fd2a6d262
Aimless <65621195+aimlesx@users.noreply.github.com>
2026-01-11 18:35:20 +0100

    fix: cursor

--- nix/modules/ide.nix
+++ nix/modules/ide.nix
@@ -1,11 +1,14 @@
 # IDE configurations: Cursor, VS Code, Roocode
-{ ... }:
+{ lib, ... }:
 
+let
+  cursorSettingsSource = ../configs/cursor/settings.json;
+  cursorKeybindingsSource = ../configs/cursor/keybindings.json;
+in
 {
   home.file = {
-    # Cursor IDE
-    ".config/Cursor/User/settings.json".source = ../configs/cursor/settings.json;
-    ".config/Cursor/User/keybindings.json".source = ../configs/cursor/keybindings.json;
+    # Cursor keybindings (symlink is fine for this)
+    ".config/Cursor/User/keybindings.json".source = cursorKeybindingsSource;
 
     # VS Code / Code OSS
     ".config/Code/User/settings.json".source = ../configs/vscode/settings.json;
@@ -21,5 +24,13 @@
       recursive = true;
     };
   };
+
+  # Copy Cursor settings instead of symlinking so Cursor can modify it
+  # Changes made in Cursor will persist until next `home-manager switch`
+  home.activation.copyCursorSettings = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    $DRY_RUN_CMD mkdir -p $HOME/.config/Cursor/User
+    $DRY_RUN_CMD cp -f ${cursorSettingsSource} $HOME/.config/Cursor/User/settings.json
+    $DRY_RUN_CMD chmod 644 $HOME/.config/Cursor/User/settings.json
+  '';
 }
 
